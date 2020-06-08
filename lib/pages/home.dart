2b36a57c8f1de5b10fe0a742e20aed82b0f5f83b
中村駿輝 <shnklife@gmail.com>
2020-06-08 14:29:10 +0900

    下から入力フォームを出す

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,146 +12,160 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final bottomSpace = MediaQuery.of(context).viewInsets.bottom;
+
     return Scaffold(
-      body: ListView(
-        children: <Widget>[
-          Column(
-            children: <Widget>[
-              // 画面上部の記録表示
-              Container(
-                child: Row(
+      body: SingleChildScrollView(
+          reverse: true,
+          child: Container(
+            margin: EdgeInsets.only(bottom: bottomSpace),
+            child: Column(
+              children: <Widget>[
+                Column(
                   children: <Widget>[
-                    recordText(time: "10s", record: "0"),
-                    recordText(time: "60s", record: "0"),
-                    recordText(time: "ENDRESS", record: "0"),
-                  ],
-                ),
-              ),
-
-              // タイトル
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Text(
-                      "Renda\nMachine",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontSize: 40
+                    // 画面上部の記録表示
+                    Container(
+                      child: Row(
+                        children: <Widget>[
+                          recordText(time: "10s", record: "0"),
+                          recordText(time: "60s", record: "0"),
+                          recordText(time: "ENDRESS", record: "0"),
+                        ],
                       ),
                     ),
-                  )
-                ],
-              ),
 
-              // ユーザー名
-              Container(
-                padding: EdgeInsets.only(left: 100,right: 100),
-                child: TextField(
-                  decoration: InputDecoration(
-                    enabledBorder: new OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.grey,
-                      ),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-
-              // メニュー
-              Container(
-                padding: EdgeInsets.only(top: 10,bottom: 10, left: 30, right: 30),
-                child: Row(
-                  children: <Widget>[
-                    menuItem(menu: "10s"),
-                    menuItem(menu: "60s"),
-                    menuItem(menu: "ENDRESS"),
-                  ],
-                ),
-              ),
-
-              // PLAYボタン
-              Container(
-                  padding: EdgeInsets.only(bottom: 10, left: 30, right: 30),
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                          child: Container(
-                            height: 70,
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.red),
+                    // タイトル
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Text(
+                            "Renda\nMachine",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 40
                             ),
-                            child: RaisedButton(
-                              onPressed: (){
-                                Navigator.push(
-                                  context,
-                                  CupertinoPageRoute(
-                                    builder: (context) => Play(time: time,),
-                                  ),
-                                );
-                              },
-                              color: Colors.white,
-                              child: Text(
-                                  "PLAY!"
-                              ),
-                            ),
-                          )
-                      )
-                    ],
-                  )
-              ),
-
-              // 画面下部
-              Container(
-                padding: EdgeInsets.only(left: 10, right: 10),
-                margin: EdgeInsets.only(top: 60),
-                child: Row(
-                  children: <Widget>[
-                    // 作成者情報
-                    Expanded(
-                      child: Text(
-                        "FONT:\n"
-                            "Isurus Labs\n\n"
-                            "ICON:\n"
-                            "Yukichi\n\n"
-                            "SPECIAK THANKS:\n"
-                            "Yukichi, @real_onesc\n\n"
-                            "(c) 2018 sinProject inc.",
-                      ),
-                    ),
-
-                    // ランキング
-                    Expanded(
-                        child: Container(
-                          padding: EdgeInsets.all(10),
-                          height: 200,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                "Leaderboard",
-                                textAlign: TextAlign.start,
-                              ),
-                              Text(
-                                  "1."
-                              )
-                            ],
-                          ),
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.red),
                           ),
                         )
-                    )
-                  ],
-                ),
-              ),
+                      ],
+                    ),
 
-            ],
-          )        ],
+                    // ユーザー名
+//              Container(
+//                padding: EdgeInsets.only(left: 100,right: 100),
+//                child: TextField(
+//                  decoration: InputDecoration(
+//                    enabledBorder: new OutlineInputBorder(
+//                      borderSide: BorderSide(
+//                        color: Colors.grey,
+//                      ),
+//                    ),
+//                    focusedBorder: OutlineInputBorder(
+//                      borderSide: BorderSide(
+//                      ),
+//                    ),
+//                  ),
+//                ),
+//              ),
+                    ListTile(
+                      onTap: () {
+                        _showModalPicker(context);
+                      },
+                    ),
+
+                    // メニュー
+                    Container(
+                      padding: EdgeInsets.only(top: 10,bottom: 10, left: 30, right: 30),
+                      child: Row(
+                        children: <Widget>[
+                          menuItem(menu: "10s"),
+                          menuItem(menu: "60s"),
+                          menuItem(menu: "ENDRESS"),
+                        ],
+                      ),
+                    ),
+
+                    // PLAYボタン
+                    Container(
+                        padding: EdgeInsets.only(bottom: 10, left: 30, right: 30),
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                                child: Container(
+                                  height: 70,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.red),
+                                  ),
+                                  child: RaisedButton(
+                                    onPressed: (){
+                                      Navigator.push(
+                                        context,
+                                        CupertinoPageRoute(
+                                          builder: (context) => Play(time: time,),
+                                        ),
+                                      );
+                                    },
+                                    color: Colors.white,
+                                    child: Text(
+                                        "PLAY!"
+                                    ),
+                                  ),
+                                )
+                            )
+                          ],
+                        )
+                    ),
+
+                    // 画面下部
+                    Container(
+                      padding: EdgeInsets.only(left: 10, right: 10),
+                      margin: EdgeInsets.only(top: 60),
+                      child: Row(
+                        children: <Widget>[
+                          // 作成者情報
+                          Expanded(
+                            child: Text(
+                              "FONT:\n"
+                                  "Isurus Labs\n\n"
+                                  "ICON:\n"
+                                  "Yukichi\n\n"
+                                  "SPECIAK THANKS:\n"
+                                  "Yukichi, @real_onesc\n\n"
+                                  "(c) 2018 sinProject inc.",
+                            ),
+                          ),
+
+                          // ランキング
+                          Expanded(
+                              child: Container(
+                                padding: EdgeInsets.all(10),
+                                height: 200,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      "Leaderboard",
+                                      textAlign: TextAlign.start,
+                                    ),
+                                    Text(
+                                        "1."
+                                    )
+                                  ],
+                                ),
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.red),
+                                ),
+                              )
+                          )
+                        ],
+                      ),
+                    ),
+
+                  ],
+                )        ],
+            ),
+          )
       )
+
     );
   }
 
@@ -209,6 +223,27 @@ class _MyHomePageState extends State<MyHomePage> {
           borderRadius: BorderRadius.circular(10),
         ),
       )
+    );
+  }
+
+  void _showModalPicker(BuildContext context) {
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: 100,
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: TextFormField()
+              ),
+              FlatButton(
+                child: Text("ok"),
+              )
+            ],
+          ),
+        );
+      },
     );
   }
 }
